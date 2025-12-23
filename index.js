dd3a1ce921728865c455e595915b016d15d95a17
ryaan540! <rkinyua841@gmail.com>
2025-12-23 12:11:05 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 
 
 
+
+
+const Datejs = require('datejs');
+
+
+function combineUsers(...args) {
+    
+    const combinedObject = {
+        users: []
+    };
+
+    
+    args.forEach(userArray => {
+        combinedObject.users.push(...userArray);
+    });
+
+    
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+   
+    return combinedObject;
+}
+
+
+const array1 = ["Alice", "Bob"];
+const array2 = ["Charlie", "Dave"];
+const array3 = ["Eve"];
+
+console.log(combineUsers(array1, array2, array3));
+
+
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
